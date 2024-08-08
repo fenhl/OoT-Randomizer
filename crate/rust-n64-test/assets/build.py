16b0899da50c025e50376812a38db604c0f78125
Fenhl <fenhl@fenhl.net>
2024-08-08 03:33:31 +0000

    Upgrade dependencies; Pin Rust version to avoid nightly breakage

--- crate/rust-n64-test/assets/build.py
+++ crate/rust-n64-test/assets/build.py
@@ -1,8 +1,12 @@
 import os
 import subprocess
 
+TOOLCHAIN = 'nightly-2024-08-07' # bad: 2024-08-08
+
+subprocess.run(['rustup', 'toolchain', 'install', TOOLCHAIN], check=True)
+subprocess.run(['rustup', 'component', 'add', 'rust-src', '--toolchain', TOOLCHAIN], check=True)
 subprocess.run(
-    ['cargo', '+nightly', 'build', '-Zbuild-std=core', '-Zbuild-std-features=compiler-builtins-mem', '--release', '--package=rust-n64-test', '--target=crate/rust-n64-test/assets/mips-ultra64-cpu.json'],
+    ['cargo', f'+{TOOLCHAIN}', 'build', '-Zbuild-std=core', '-Zbuild-std-features=compiler-builtins-mem', '--release', '--package=rust-n64-test', '--target=crate/rust-n64-test/assets/mips-ultra64-cpu.json'],
     env={**os.environ, 'RUSTFLAGS': '-Csymbol-mangling-version=v0'}, # symbol-mangling-version (https://github.com/rust-lang/rust/issues/60705) required for armips to accept the symbols
     check=True,
 )
