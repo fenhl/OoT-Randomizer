4f2c9755ec315a534f24fc1f79cc6f7c5bc53dd6
Fenhl <fenhl@fenhl.net>
2024-08-27 06:13:12 +0000

    Logic for repeatable blue warp ToD changes

--- World.py
+++ World.py
@@ -585,8 +585,8 @@ class World:
             if 'time_passes' in region:
                 new_region.time_passes = region['time_passes']
                 new_region.provides_time = TimeOfDay.ALL
-            if new_region.name == 'Ganons Castle Grounds':
-                new_region.provides_time = TimeOfDay.DAMPE
+            if 'provides_time' in region:
+                new_region.provides_time = getattr(TimeOfDay, region['provides_time'])
             if 'locations' in region:
                 for location, rule in region['locations'].items():
                     new_location = LocationFactory(location)
