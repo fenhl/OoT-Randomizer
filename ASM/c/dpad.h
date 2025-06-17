7ec317f0b5362362b2da9ffda5ef60452d888fd6
Fenhl <fenhl@fenhl.net>
2025-06-17 14:40:56 +0000

    Disable Zelda's letter D-pad binding once turned in

--- ASM/c/dpad.h
+++ ASM/c/dpad.h
@@ -69,12 +69,17 @@ extern uint8_t CFG_CHILD_TRADE_SHUFFLE;
 #define CAN_USE_OCARINA     (z64_game.pause_ctxt.state == PAUSE_STATE_OFF && \
                             (z64_file.items[Z64_SLOT_OCARINA] == Z64_ITEM_FAIRY_OCARINA || z64_file.items[Z64_SLOT_OCARINA] == Z64_ITEM_OCARINA_OF_TIME) && \
                             !z64_game.restriction_flags.ocarina && \
-                            ((z64_link.state_flags_1 & BLOCK_ITEMS) == 0) && \
-                            (!(z64_link.state_flags_3 & (1 << 7))) && \
+                            (z64_link.state_flags_1 & BLOCK_ITEMS) == 0 && \
+                            !(z64_link.state_flags_3 & (1 << 7)) && \
                             z64_game.bombchuBowlingStatus == 0 && \
                             z64_game.shootingGalleryStatus == 0)
 
-#define CAN_USE_CHILD_TRADE (z64_game.pause_ctxt.state == PAUSE_STATE_OFF && z64_file.items[Z64_SLOT_CHILD_TRADE] >= Z64_ITEM_WEIRD_EGG && z64_file.items[Z64_SLOT_CHILD_TRADE] <= Z64_ITEM_MASK_OF_TRUTH && !z64_game.restriction_flags.trade_items && ((z64_link.state_flags_1 & BLOCK_ITEMS) == 0))
+#define CAN_USE_CHILD_TRADE (z64_game.pause_ctxt.state == PAUSE_STATE_OFF && \
+                            z64_file.items[Z64_SLOT_CHILD_TRADE] >= Z64_ITEM_WEIRD_EGG && \
+                            z64_file.items[Z64_SLOT_CHILD_TRADE] <= Z64_ITEM_MASK_OF_TRUTH && \
+                            !z64_game.restriction_flags.trade_items && \
+                            (z64_link.state_flags_1 & BLOCK_ITEMS) == 0 && \
+                            (z64_file.items[Z64_SLOT_CHILD_TRADE] != Z64_ITEM_ZELDAS_LETTER || (z64_file.inf_table[7] & 0x40) == 0))
 
 void handle_dpad();
 void draw_dpad_and_menu_utilities();
